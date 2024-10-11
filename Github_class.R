c77020db5e74b21c6fd6568144d91a734df660a7
loisluijben <lois.luijben@gmail.com>
2024-10-11 17:08:12 -0300

    Update Github_class.R

--- Github_class.R
+++ Github_class.R
@@ -1,5 +1,5 @@
 #Hello, will you ever read this again? What's up then broski#
-#Currently in class in French Guyana, sitting next to Pierrot. 
+#Currently in class in French Guyana, sitting next to Pierrot. Conflict or not?
 #Julien is giving us a class on Github usage. I'm feeling alright, the air is more fresh today as it rained tonight#
 
 2+2
